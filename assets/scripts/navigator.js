4691885f3e140108880c5aad897505245f2714ae
RahilV <rahil.v@somaiya.edu>
2022-09-24 11:58:07 +0530

    social links anim

--- assets/scripts/navigator.js
+++ assets/scripts/navigator.js
@@ -20,18 +20,102 @@ window.onload = () => {
             }
             btns[0].classList.add('active');
         }
+        const toProjectSection = () => {
+            // we create text
+            document.getElementById('wefoot1text').classList.add("animate");
+            document.getElementById('wefoot1text').classList.remove("unanimate");
 
+            // we create left arrow
+            document.getElementById('arrowleft').classList.add("arrowleftanimate");
+            document.getElementById('arrowleft').classList.remove("arrowleftunanimate");
+
+            // we creat right arrow
+            document.getElementById('arrowright').classList.add("arrowrightanimate");
+            document.getElementById('arrowright').classList.remove("arrowrightunanimate");
+
+            // proj left arrow
+            document.getElementById('leftProject').classList.add("projarrowanim");
+            document.getElementById('leftProject').classList.remove("projarrowunanim");
+
+            // proj right arrow
+            document.getElementById('rightProject').classList.add("projarrowanim");
+            document.getElementById('rightProject').classList.remove("projarrowunanim");
+        }
+
+        const toWeCreatSection = () => {
+            // we create text
+            document.getElementById('wefoot1text').classList.add("unanimate");
+            document.getElementById('wefoot1text').classList.remove("animate");
+
+            // we create left arrow
+            document.getElementById('arrowleft').classList.add("arrowleftunanimate");
+            document.getElementById('arrowleft').classList.remove("arrowleftanimate");
+
+            // we create right arrow
+            document.getElementById('arrowright').classList.add("arrowrightunanimate");
+            document.getElementById('arrowright').classList.remove("arrowrightanimate");
+
+            // proj left arrow
+            document.getElementById('leftProject').classList.add("projarrowunanim");
+            document.getElementById('leftProject').classList.remove("projarrowanim");
+
+            // proj right arrow
+            document.getElementById('rightProject').classList.add("projarrowunanim");
+            document.getElementById('rightProject').classList.remove("projarrowanim");
+        }
+        const loadTypeAnim = () => {
+            // typing animation scale up animation
+            document.getElementById('typeanim').play();
+            document.getElementById('type').classList.add("showtype");
+            document.getElementById('type').classList.remove("hidetype");
+        }
+
+        const hideTypeAnim = () => {
+            // typing animation scale up animation
+            document.getElementById('type').classList.add("hidetype");
+            document.getElementById('typeanim').stop();
+        }
+
+        const loadFooterAnim = () => {
+            document.getElementById('social1').classList.add("fallanimation");
+            document.getElementById('social2').classList.add("fallanimation");
+            document.getElementById('social3').classList.add("fallanimation");
+            document.getElementById('social1').classList.remove("raiseanimation");
+            document.getElementById('social2').classList.remove("raiseanimation");
+            document.getElementById('social3').classList.remove("raiseanimation");
+            document.getElementById('footsocial1').classList.add("footersocialanim");
+            document.getElementById('footsocial2').classList.add("footersocialanim");
+            document.getElementById('footsocial3').classList.add("footersocialanim");
+        }
+        const loadSocialLinksAnim = () => {
+            document.getElementById('social1').classList.add("raiseanimation");
+            document.getElementById('social2').classList.add("raiseanimation");
+            document.getElementById('social3').classList.add("raiseanimation");
+            document.getElementById('social1').classList.remove("fallanimation");
+            document.getElementById('social2').classList.remove("fallanimation");
+            document.getElementById('social3').classList.remove("fallanimation");
+            document.getElementById('footsocial1').classList.remove("footersocialanim");
+            document.getElementById('footsocial2').classList.remove("footersocialanim");
+            document.getElementById('footsocial3').classList.remove("footersocialanim");
+        }
         const gotoNum = (index) => {
             if ((index != current) && !animation_state) {
-                if (current == 1 && index == 2){
-                    document.getElementById('wefoot1').classList.add("animate");
-                    document.getElementById('arrowleft').classList.add("arrowleftanim");
-                    document.getElementById('wefoot1').classList.remove("unanimate");
+                if(current == 0 && index == 1){
+                    loadTypeAnim();
                 }
-                else if (index == 1 && current == 2){
-                    console.log(current);
-                    document.getElementById('wefoot1').classList.add("unanimate");
-                    // document.getElementById('wefoot1').classList.remove("animate");
+                else if (current == 1 && index == 2){
+                    toProjectSection();
+                    hideTypeAnim();
+                }
+                else if (current == 2 && index == 1){
+                    toWeCreatSection();
+                    loadTypeAnim();
+                }
+                else if(current ==2 && index == 3){
+                    loadFooterAnim();
+                }
+                else if(current == 3){
+                    loadSocialLinksAnim();
                 }
                 animation_state = true;
                 setTimeout(() => animation_state = false, 500);
